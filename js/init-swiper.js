b92d07bdd5a564da449e60ef8e81ad79a5840faf
wuwenxing <1428560819@qq.com>
2018-06-19 20:58:16 +0800

    模块化2.0

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,16 +1,18 @@
-var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    //direction: 'vertical',
-    loop: true,
+! function() {
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        //direction: 'vertical',
+        loop: true,
 
-    // If we need pagination
+        // If we need pagination
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
 
-    // And if we need scrollbar
+        // And if we need scrollbar
 
-})
+    })
+}.call()
